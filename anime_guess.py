9448c2a8e8a27e6617806d2d769f98b13f62c90a
riyouk <gostoftheuchiha1@gmail.com>
2025-03-11 10:16:40 +0530

    adding a new file to a my second repo

---
+++ anime_guess.py
@@ -0,0 +1,9 @@
+def anime (name):
+    match name:
+        case "one_piece" :
+            print("the main protaganist is MONKEY D LUFFY !")
+        case "naruto":
+            print("the main protaganist is UZUMAKI NARUTO !")
+        case "bleach":
+            print("the main protaganist is KOROSAKI ICHIGO !")
+anime("one_piece")
